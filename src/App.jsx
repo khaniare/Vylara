70a19be9685b4913359ac65a23967341eebb437e
khanindrare <khanindrare@gmail.com>
2024-07-04 18:18:25 +0000

    🪰 0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';import Windows from './Windows'; // Example component import
+// import Linux from 'src/assets/blog/linux.jsx'; // Example component import
+// import iOS from 'src/assets/blog/IOS.jsx'; // Example component import
+// import Android from 'src/assets/blog/android.jsx'; // Example component import
 
 function Blog() {
-
-  return (
-    <div>
-      <h1>
-        <pre> </pre><span style={{ fontSize: '0.8em', fontWeight: '300' }}>Welcome to...   </span>
-        <b style={{
+  const [text, setText] = useState(
+    <h1>
+      <pre> </pre>
+      <span style={{ fontSize: '0.8em', fontWeight: '300', color: '#999' }}>
+        Welcome to...
+        <br />
+      </span>
+      <b
+        style={{
           fontSize: '5em',
           fontWeight: '600',
           background: 'linear-gradient(to right, #b7d8c3, #bdd4c5, #444444)',
           WebkitBackgroundClip: 'text',
-          WebkitTextFillColor: 'transparent'
-        }}>Vylara</b>
-      </h1>
-
-    </div>
+          WebkitTextFillColor: 'transparent',
+          marginLeft: '0.5em',
+        }}
+      >
+        Vylara
+      </b>
+    </h1>
   );
+
+  const handleClick = () => {
+    setText(
+    
+      <p>
+      <span style={{ fontSize: '1em', fontWeight: '300', color: '#999' }}>
+        Knowing why you're here!
+      </span>
+    </p>
+    
+    
+    
+    
+    
+    
+    );
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  return <div>{text}</div>;
 }
 
 export default Blog;
